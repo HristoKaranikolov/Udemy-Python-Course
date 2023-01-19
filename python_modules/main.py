1b88c14be0229ed0cb239ea64846b37316746060
HristoKaranikolov <104217165+HristoKaranikolov@users.noreply.github.com>
2023-01-19 13:04:01 +0200

    Exercises for modules in Python
    Exercises with random built in module

---
+++ python_modules/main.py
@@ -0,0 +1,7 @@
+from math_operations import multiply, divide
+from shopping.shopping_cart import buy
+
+print(multiply(2, 5))
+print(divide(6, 2))
+
+print(buy('Motorcycle'))
---
+++ python_modules/random_module.py
@@ -0,0 +1,13 @@
+from random import shuffle, random, choice, randint
+
+# Shuffling a list
+my_list = [1, 2, 3, 4, 5]
+shuffle(my_list)
+print(my_list)
+
+# Random number
+random_int = randint(0, 20)
+print(random_int)
+
+# Random choosing
+print(choice(my_list))
